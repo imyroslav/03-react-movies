5203dc25f91660f49f2dd1c31938c7ad4b3aca0e
imyroslav <imyroslav@ukr.net>
2025-05-29 22:57:13 +0200

    search-bar-update-1

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import css from "./App.module.css"
+import SearchBar from "../SearchBar/SearchBar";
+
+
+export default function App() {
+    return (
+        <div className={css.app}>
+            <SearchBar />
+        </div>
+        
+    )
+}

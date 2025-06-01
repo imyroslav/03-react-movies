e576649218635272aca43874ffd8cf4f9f4431be
imyroslav <imyroslav@ukr.net>
2025-06-01 21:54:37 +0200

    updates-01.06.2025

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,36 @@
-import axios from "axios";
+import { useState } from "react";
 
 import css from "./App.module.css"
 import SearchBar from "../SearchBar/SearchBar";
-// import MovieGrid from "../MovieGrid/MovieGrid";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import MovieModal from "../MovieModal/MovieModal";
+import { fetchMovies } from "../../services/movieService";
+import { type Movie } from "../../types/movie"
 
 
 export default function App() {
 
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null)
+
+    const openModal = (movies: Movie) => {
+        setSelectedMovie(movies);
+    };
+
+    const closeModal = () => {
+        setSelectedMovie(null);
+    };
+
     const handleSearch = async (newQuery: string) => {
-        const result = await axios.get(`http://hn.algolia.com/api/v1/search?query=${newQuery}`);
-        console.log(result)
+        const newMovies = await fetchMovies(newQuery);
+        setMovies(newMovies)
     }
 
     return (
         <div className={css.app}>
             <SearchBar onSearch={handleSearch} />
-            {/* <MovieGrid /> */}
+            <MovieGrid onSelect={openModal} movies={movies} /> 
+            <MovieModal onClose={closeModal} movie={selectedMovie} />
         </div>
         
     )
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,6 +1,26 @@
+import { type Movie } from "../types/movie";
 
+import axios from "axios";
 
-export default function fetchMovies() {
-    return
+export interface GetMovies {
+    results: Movie[];
+}
+
+export const reqConfig = {
+    url: "https://api.themoviedb.org/3/search/movie/550?api_key=427fa683bb09806c94c514056e83d736",
+    headers: {
+        accept: "aplication/json",
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MjdmYTY4M2JiMDk4MDZjOTRjNTE0MDU2ZTgzZDczNiIsIm5iZiI6MTcxNzIwNjgzNS40NDYsInN1YiI6IjY2NWE3ZjMzYTdmOTgzODgxMDZlN2M3YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qCInCE6xYmdVuc7fLaX_-q2WoVg2bjui_RoB5WrsvE8`
+    }
+}
+
+export const fetchMovies = async(newQuery: string): Promise<Movie[]> => {
+    const result = await axios.get<GetMovies>(
+        `${reqConfig.url}?query=${newQuery}`,
+        reqConfig
+    );
+    
+    
+    return result.data.results;
 }
 

a561cd67fb9754e45da903cc2766e61f42f55cb0
imyroslav <imyroslav@ukr.net>
2025-06-02 19:33:24 +0200

    comment-02-06-2025

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -7,7 +7,7 @@ export interface GetMovies {
 }
 
 export const reqConfig = {
-    url: "https://api.themoviedb.org/3/search/movie/550?api_key=427fa683bb09806c94c514056e83d736",
+    url: "https://api.themoviedb.org/3/search/movie",
     headers: {
         accept: "aplication/json",
         Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MjdmYTY4M2JiMDk4MDZjOTRjNTE0MDU2ZTgzZDczNiIsIm5iZiI6MTcxNzIwNjgzNS40NDYsInN1YiI6IjY2NWE3ZjMzYTdmOTgzODgxMDZlN2M3YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qCInCE6xYmdVuc7fLaX_-q2WoVg2bjui_RoB5WrsvE8`
@@ -20,7 +20,7 @@ export const fetchMovies = async(newQuery: string): Promise<Movie[]> => {
         reqConfig
     );
     
-    
+    console.log(result);
     return result.data.results;
 }
 
